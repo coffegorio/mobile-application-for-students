2f9435d13ac931b1ac9a0da3aa0ec97c6fe8b570
coffegorio <coffegorio@vk.com>
2024-12-21 06:15:53 +0300

    added pagination

--- mobile-application-for-students/ViewControllers/NewStudentScreenVC.swift
+++ mobile-application-for-students/ViewControllers/NewStudentScreenVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SnapKit
 
 class NewStudentScreenVC: UIViewController {
     
@@ -33,6 +34,49 @@ class NewStudentScreenVC: UIViewController {
         return collection
         
     }()
+    
+    private var pagers: [UIView] = []
+    private var currentSlide = 0
+    private var widthAnchor: NSLayoutConstraint?
+    
+    lazy var horizontalStack: UIStackView = {
+        
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.alignment = .center
+        stack.spacing = 0
+        stack.distribution = .equalSpacing
+        
+        return stack
+    }()
+    
+    lazy var nextPageButton: UIView = {
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(nextSlide))
+       
+        let nextImg = UIImageView()
+        nextImg.image = UIImage(systemName: "chevron.right.circle.fill")
+        nextImg.contentMode = .scaleAspectFit
+        nextImg.tintColor = Styles.Colors.appBlackColor
+        nextImg.translatesAutoresizingMaskIntoConstraints = false
+        nextImg.widthAnchor.constraint(equalToConstant: 45).isActive = true
+        nextImg.heightAnchor.constraint(equalToConstant: 45).isActive = true
+        
+        let button = UIView()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        button.isUserInteractionEnabled = true
+        button.addGestureRecognizer(tapGesture)
+        button.addSubview(nextImg)
+        
+        nextImg.centerXAnchor.constraint(equalTo: button.centerXAnchor).isActive = true
+        nextImg.centerYAnchor.constraint(equalTo: button.centerYAnchor).isActive = true
+        
+        
+        return button
+        
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,9 +84,44 @@ class NewStudentScreenVC: UIViewController {
         setupUI()
         setupCollection()
         setupConstraints()
-        setupActions()
+        setControll()
         
     }
+    
+    private func setControll() {
+        // Добавляем horizontalStack на экран
+        view.addSubview(horizontalStack)
+        
+        // Создаем pagerStack
+        let pagerStack = UIStackView()
+        pagerStack.axis = .horizontal
+        pagerStack.spacing = 5
+        pagerStack.alignment = .center
+        pagerStack.distribution = .fill
+
+        // Добавляем точки-индикаторы в pagerStack
+        for tag in 1...sliderData.count {
+            let pager = UIView()
+            pager.tag = tag
+            pager.backgroundColor = Styles.Colors.appBlackColor
+            pager.layer.cornerRadius = 5
+            pager.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(scrollToSlide(sender: ))))
+            self.pagers.append(pager)
+            pagerStack.addArrangedSubview(pager)
+        }
+
+        // Добавляем pagerStack в horizontalStack
+        horizontalStack.addArrangedSubview(pagerStack)
+        horizontalStack.addArrangedSubview(nextPageButton)
+
+        // Устанавливаем ограничения для horizontalStack
+        horizontalStack.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(40)
+            make.trailing.equalToSuperview().inset(40)
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).offset(-20)
+        }
+    }
+
     
     
     private func setupUI() {
@@ -66,10 +145,17 @@ class NewStudentScreenVC: UIViewController {
         
     }
     
-    private func setupActions() {
-        
+    @objc private func scrollToSlide(sender: UIGestureRecognizer) {
+        if let index = sender.view?.tag {
+            collectionView.scrollToItem(at: IndexPath(item: index - 1, section: 0), at: .centeredHorizontally, animated: true)
+            currentSlide = index - 1
+        }
     }
 
+    @objc private func nextSlide() {
+        
+    }
+    
 }
 
 extension NewStudentScreenVC: UICollectionViewDelegate, UICollectionViewDataSource {
@@ -88,4 +174,40 @@ extension NewStudentScreenVC: UICollectionViewDelegate, UICollectionViewDataSour
         }
         return UICollectionViewCell()
     }
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        currentSlide = indexPath.item
+        
+        pagers.forEach { page in
+            let tag = page.tag
+            let viewTag = indexPath.row + 1
+            
+            // Удаляем только нужные ограничения
+            if let widthConstraint = page.constraints.first(where: { $0.firstAttribute == .width }) {
+                page.removeConstraint(widthConstraint)
+            }
+            
+            // Настраиваем вид и добавляем новые ограничения
+            if viewTag == tag {
+                UIView.animate(withDuration: 0.3) {page.layer.opacity = 1}
+                widthAnchor = page.widthAnchor.constraint(equalToConstant: 20)
+            } else {
+                UIView.animate(withDuration: 0.3) {page.layer.opacity = 0.5}
+                widthAnchor = page.widthAnchor.constraint(equalToConstant: 10)
+            }
+            widthAnchor?.isActive = true
+            page.heightAnchor.constraint(equalToConstant: 10).isActive = true
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+
 }
